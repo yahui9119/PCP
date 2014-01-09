73c209590ca0049a9eff8822f6272eb129af68bb
yahui9119 <yahui9119@qq.com>
2014-01-09 18:37:31 +0800

    p2pdemo

---
+++ P2Pnoclip/Client/FormatterHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace Client
+{
+    /// <summary>
+    /// FormatterHelper 序列化，反序列化消息的帮助类
+    /// </summary>
+    public class FormatterHelper
+    {
+        public static byte[] Serialize(object obj)
+        {
+
+
+            BinaryFormatter binaryF = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream(1024 * 10);
+            binaryF.Serialize(ms, obj);
+            ms.Seek(0, SeekOrigin.Begin);
+
+            byte[] buffer = new byte[(int)ms.Length];
+            ms.Read(buffer, 0, buffer.Length);
+            ms.Close();
+
+
+            return buffer;
+        }
+
+        public static object Deserialize(byte[] buffer)
+        {
+            BinaryFormatter binaryF = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream(buffer, 0, buffer.Length, false);
+
+            object obj = binaryF.Deserialize(ms);
+
+            ms.Close();
+
+            return obj;
+        }
+    }
+  
+}
---
+++ P2Pnoclip/Client/P2PConsts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Client
+{
+    /// <summary>
+
+    /// P2P共享数据类
+    /// </summary>
+    public class P2PConsts
+    {
+        /// <summary>
+        /// UDP服务器监听端口
+        /// </summary>
+        public const int UDP_SRV_PORT = 2280;
+
+
+        /// <summary>
+        ///TCP服务器监听端口
+        /// </summary>
+        public const int TCP_SRV_PORT = 2000;
+    }
+}
